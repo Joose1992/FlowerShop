7dbcfc3f5b65f70cde2bf955caf70bbaa076c44c
Jose Montanez <josemontanez1992@gmail.com>
2023-10-04 10:44:31 -0600

    Add project files.

---
+++ FlowerShop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowerShop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
